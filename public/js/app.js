4f682707f41dae03b4ef87ff7de93267e5b2cca0
khairo14 <eldrin.bradecina@gmail.com>
2022-07-06 02:02:50 +0800

    scanning adjustment for new cust

--- public/js/app.js
+++ public/js/app.js
@@ -2113,13 +2113,14 @@ $(document).ready(function () {
             var prod = "<tr>" + "<td class='py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6'>" + "<p class='w-12 truncate overflow-clip'>" + pcode + "</p>" + "</td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'>" + gtin + "</td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'>" + plu + "</td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'><p class='w-12 truncate overflow-clip'>" + pname + "</p></td>" + "<td class='relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6'>" + "<a href='#' class='rm_prod text-indigo-600 hover:text-indigo-900'>" + "<svg xmlns='http://www.w3.org/2000/svg' class='w-6 h-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'>" + "<path stroke-linecap='round' stroke-linejoin='round' d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16' />" + "</svg>" + "</a>" + "</td>" + "</tr>";
             $("#scnproducts_body").append(prod);
           } else if (result.status == 2) {
+            var gtin = result.message['message2'];
             $("#scan_pcode").val('');
             $("#scan_pcode").focus();
-            $(".scan_pcode_message").text(result.message);
+            $(".scan_pcode_message").text(result.message['message1']);
             setTimeout(function () {
               $(".scan_pcode_message").text('');
             }, 5000);
-            var prod = "<tr>" + "<td class='py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6'>" + "<p class='w-12 truncate overflow-clip'>" + pcode + "</p>" + "</td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'></td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'></td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'></td>" + "<td class='relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6'>" + "<a href='#' class='rm_prod text-indigo-600 hover:text-indigo-900'>" + "<svg xmlns='http://www.w3.org/2000/svg' class='w-6 h-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'>" + "<path stroke-linecap='round' stroke-linejoin='round' d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16' />" + "</svg>" + "</a>" + "</td>" + "</tr>";
+            var prod = "<tr>" + "<td class='py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6'>" + "<p class='w-12 truncate overflow-clip'>" + pcode + "</p>" + "</td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'>" + gtin + "</td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'></td>" + "<td class='px-3 py-4 text-sm text-gray-500 whitespace-nowrap'></td>" + "<td class='relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6'>" + "<a href='#' class='rm_prod text-indigo-600 hover:text-indigo-900'>" + "<svg xmlns='http://www.w3.org/2000/svg' class='w-6 h-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'>" + "<path stroke-linecap='round' stroke-linejoin='round' d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16' />" + "</svg>" + "</a>" + "</td>" + "</tr>";
             $("#scnproducts_body").append(prod);
           } else {
             $("#scan_pcode").val('');
@@ -4579,13 +4580,38 @@ $(document).ready(function () {
 $(document).ready(function () {
   $("#flter").on("change", function () {
     var fltr = $(this).val();
+    var cx = $("#exist_cust1 option:selected").val();
 
     if (fltr.length > 1) {
       $("#plu_fltr").show();
       $("#rcvd_dte_fltr").show();
     } else {
       if (fltr == 1) {
-        $("#plu_fltr").show();
+        $.ajax({
+          url: "/stocks/get-plu",
+          headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          },
+          method: 'post',
+          data: {
+            "cx": cx
+          },
+          success: function success(result) {
+            if (result.status == 1) {
+              var plu = result.plu;
+              var pl_arr = [];
+              $.map(plu, function (val, i) {
+                pl_arr.push(val);
+              });
+              var plu = pl_arr.sort();
+              plu.forEach(function (pl) {
+                var option = "<option value='" + pl + "'>" + pl + "</option>";
+                $("#fltr_plu").append(option);
+              });
+              $("#plu_fltr").show();
+            }
+          }
+        });
       } else {
         $("#plu_fltr").hide();
       }
